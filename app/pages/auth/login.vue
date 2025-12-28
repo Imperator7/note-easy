8800d3e3c6bf0f670bc1b48507b50a033bf2a13c
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-12-29 03:44:24 +0700

    refactor: add demo message for demo login

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -26,33 +26,42 @@ async function login() {
 
 <template>
   <div class="flex flex-1 items-center justify-center bg-gray-200">
-    <form
-      @submit.prevent="login"
-      class="flex flex-col gap-4 bg-gray-300 rounded max-w-100 p-4 -translate-y-20"
-    >
-      <input
-        v-model="credentials.email"
-        type="email"
-        placeholder="Email"
-        class="border p-2 rounded"
-      />
-      <input
-        v-model="credentials.password"
-        type="password"
-        placeholder="Password"
-        class="border p-2 rounded"
-      />
-      <p>
-        Don't have an account?
-        <NuxtLink to="/auth/register" class="underline">register one</NuxtLink>
-      </p>
-
-      <button
-        type="submit"
-        class="bg-green-400 py-2 px-1 font-bold rounded cursor-pointer"
+    <div class="-translate-y-20 flex flex-col gap-4">
+      <div class="p-4 bg-amber-200 border border-amber-400 rounded">
+        <p class="text-center font-bold">For demo only</p>
+        <p><span class="font-medium">email:</span> admin@admin.com</p>
+        <p><span class="font-medium">password:</span> password</p>
+      </div>
+      <form
+        @submit.prevent="login"
+        class="flex flex-col gap-4 bg-gray-300 rounded min-w-80 p-4"
       >
-        Login
-      </button>
-    </form>
+        <input
+          v-model="credentials.email"
+          type="email"
+          placeholder="Email"
+          class="border p-2 rounded"
+        />
+        <input
+          v-model="credentials.password"
+          type="password"
+          placeholder="Password"
+          class="border p-2 rounded"
+        />
+        <!-- <p>
+          Don't have an account?
+          <NuxtLink to="/auth/register" class="underline"
+            >register one</NuxtLink
+          >
+        </p> -->
+
+        <button
+          type="submit"
+          class="bg-green-400 py-2 px-1 font-bold rounded cursor-pointer"
+        >
+          Login
+        </button>
+      </form>
+    </div>
   </div>
 </template>
