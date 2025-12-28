2b711f4ad5768f988dc94cb00f25d106646e3378
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-12-28 14:21:21 +0700

    module: add nuxt-auth-utils to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
-  modules: ['@nuxt/icon', '@pinia/nuxt'],
+  modules: ['@nuxt/icon', '@pinia/nuxt', 'nuxt-auth-utils'],
   vite: {
     plugins: [tailwindcss()],
   },
