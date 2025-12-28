f5ea0b85086456411198b9b842513706a058fa78
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-12-28 14:24:41 +0700

    feat: create client service layer for note operation

---
+++ app/composables/useNote.ts
@@ -0,0 +1,82 @@
+export const useNote = () => {
+  const fetching = ref<boolean>(false)
+  const creating = ref<boolean>(false)
+
+  const notes = useState<Note[]>('notes', () => [])
+
+  const fetchNotes = async () => {
+    fetching.value = true
+    try {
+      const { data } = await $fetch('/api/notes')
+      notes.value = [...notes.value, ...data]
+      console.log('Fetched', notes.value)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      fetching.value = false
+    }
+  }
+
+  const createNote = async (payload: {
+    note: string
+    author: string
+    category: string
+  }) => {
+    creating.value = true
+    try {
+      const { data } = await $fetch('/api/notes', {
+        method: 'POST' as any,
+        body: payload,
+      })
+      notes.value.push(data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      creating.value = false
+    }
+  }
+
+  const removeNoteById = async (payload: { id: string }) => {
+    try {
+      await $fetch(`/api/notes/${payload.id}`, {
+        method: 'DELETE' as any,
+      })
+
+      notes.value = notes.value.filter((note) => note.id !== payload.id)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const editNoteById = async (payload: {
+    id: string
+    note?: string
+    category?: string
+  }) => {
+    try {
+      const { data } = await $fetch(`/api/notes/${payload.id}`, {
+        method: 'PATCH',
+        body: {
+          note: payload.note,
+          category: payload.category,
+        },
+      })
+
+      notes.value = notes.value.map((note) =>
+        note.id === data.id ? data : note
+      )
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return {
+    notes,
+    fetchNotes,
+    fetching,
+    createNote,
+    creating,
+    removeNoteById,
+    editNoteById,
+  }
+}
