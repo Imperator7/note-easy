40838354909ff992f0335a320679ae5a08278628
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2026-01-13 01:24:28 +0700

    feat: make the note post endpoint to use the data on the session

--- server/api/notes.post.ts
+++ server/api/notes.post.ts
@@ -5,18 +5,25 @@ export default defineEventHandler(async (event) => {
 
   const postBodySchema = z.object({
     note: z.string().min(1),
-    author: z.string().min(1),
     category: z.string().min(1),
   })
 
-  const { note, author, category } = await readValidatedBody(
+  const { note, category } = await readValidatedBody(
     event,
     postBodySchema.parse
   )
 
+  const session = await getUserSession(event)
+  if (!session.user) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized request, please login and try again',
+    })
+  }
+
   return {
     success: true,
     status: 201,
-    data: createNote(note, category, author),
+    data: createNote(note, category, session.user.name, session.user.userId),
   }
 })
